bbfebc1686e8095df1fe60caca462efe2106b245
Joshua Burer <joshburer@gmail.com>
2022-11-25 20:56:26 -0500

    Add favicon and astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,11 @@ import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [image({ serviceEntryPoint: "@astrojs/image/sharp" })],
+  integrations: [image({
+    serviceEntryPoint: "@astrojs/image/sharp"
+  }), mdx()]
 });
