4b7ea985b01f34f1be229a6459fe1ba3401ca2c0
Joshua Burer <joshburer@gmail.com>
2022-11-25 22:01:14 -0500

    Fix config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from "astro/config";
 import image from "@astrojs/image";
-
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "joshburer.github.io",
+  site: "https://joshburer.github.io",
   base: "/astro-site",
   integrations: [
     image({
