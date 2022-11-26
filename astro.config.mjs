078323de744e3f769a6b294f261d0fc24e0e21b6
Joshua Burer <joshburer@gmail.com>
2022-11-26 15:25:10 -0500

    Move about to blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: "https://joshburer.github.io",
-  // base: "/astro-site",
+  base: "/astro-site",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
--- src/pages/blog/example-post.md
+++
@@ -1,8 +0,0 @@
----
-layout: ../../layouts/BlogLayout.astro
-title: "Example Blog post!"
-description: "THis is in the blog"
-date: "Nov. 25, 2022"
----
-
-# Example Post
