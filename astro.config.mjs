4aa76339e90fb8ae12657829b5634295ac3afa2b
Joshua Burer <joshburer@gmail.com>
2022-11-26 20:50:24 -0500

    font

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,7 @@ import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://joshburer.github.io",
+  site: "https://joshuaburer.com",
   integrations: [
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
