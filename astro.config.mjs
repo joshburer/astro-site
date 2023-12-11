0a0c201e2ba3ce068be47d27b49b8f44ee4c1293
Joshua Burer <joshburer@gmail.com>
2023-12-11 16:18:25 -0500

    Upgrade packages to latest

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,10 @@
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://joshuaburer.com",
   integrations: [
-    image({
-      serviceEntryPoint: "@astrojs/image/sharp",
-    }),
     mdx(),
   ],
 });
