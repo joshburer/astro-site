86780ee7b9d4ca7d2292fc3a5b06eeecd77e946b
Joshua Burer <joshburer@gmail.com>
2026-01-17 12:21:43 -0500

    Update dependencies, fix Icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://joshuaburer.com",
   integrations: [
     mdx(),
+    icon(),
   ],
 });
